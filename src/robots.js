7dbcacef9e1546333fa27f1394612b1f4088977b
Andreas Hagstroem <ahagstroem@gmail.com>
2020-04-03 12:30:09 +0200

    project added

---
+++ src/robots.js
@@ -0,0 +1,71 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Martin Sundström",
+    username: "Martin",
+    email: "martin@april.biz"
+  },
+
+  {
+    id: 2,
+    name: "Ivar Forstadius",
+    username: "Ivar",
+    email: "ivar@tv.com"
+  },
+
+  {
+    id: 3,
+    name: "Jesper Sundblad",
+    username: "Jesper",
+    email: "jesper@kvarnholmen.it"
+  },
+
+  {
+    id: 4,
+    name: "Eleonore Sager",
+    username: "Ellan",
+    email: "ellan@flx.dk"
+  },
+
+  {
+    id: 5,
+    name: "Sara Boström",
+    username: "Sara",
+    email: "sara@rk.se"
+  },
+
+  {
+    id: 6,
+    name: "Maria Furbring",
+    username: "Mia",
+    email: "maria@ssc.fr"
+  },
+
+  {
+    id: 7,
+    name: "Fred Hagström",
+    username: "Fred",
+    email: "fred@dagis.nu"
+  },
+
+  {
+    id: 8,
+    name: "Vera Hagström",
+    username: "Vera",
+    email: "vera@ridahast.nu"
+  },
+
+  {
+    id: 9,
+    name: "Tomas Lagerlöf",
+    username: "Tompa",
+    email: "tomas@alléskolan.io"
+  },
+
+  {
+    id: 10,
+    name: "Märta Frank",
+    username: "Märta",
+    email: "marta@kvv.se"
+  }
+];
